317441ee42f7f177ec7a6428212fc5b1a0789633
PANKAJ KUMAR <pankajkr.vitj@gmail.com>
2025-05-30 16:00:10 +0000

    code for the multiplication table of any number

---
+++ multiplicationTable.js
@@ -0,0 +1,7 @@
+// This Code is for the Multiplicaiton table of any number 
+// You have to just change the value of n(numbers only);
+
+let n=27;
+for(i=1;i<=10;i++){
+    console.log(n +"*"+ i + "=" + (n*i));
+}
