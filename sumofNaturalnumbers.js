8f8a528a44b82ffce83e865da81b0757da14ab15
PANKAJ KUMAR <pankajkr.vitj@gmail.com>
2025-05-30 16:35:19 +0000

    code for the sum of natural numbers.

---
+++ sumofNaturalnumbers.js
@@ -0,0 +1,19 @@
+// let n=5;
+// var sum=0;
+// for(i=1;i<=n;i++){
+//     sum=sum+i
+    
+// }
+// console.log(sum);
+
+// using function
+
+
+function sumOfNatural(n){
+    let sum=0;
+    for(i=1;i<=n;i++)
+    sum+=i;
+    return sum;
+}
+let n=4
+console.log(sumOfNatural(n));
