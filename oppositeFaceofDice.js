cadae888a1a77dd0d9656cb652eaece380901961
PANKAJ KUMAR <pankajkr.vitj@gmail.com>
2025-06-01 18:29:08 +0000

    another method

--- oppositeFaceofDice.js
+++ oppositeFaceofDice.js
@@ -1,21 +1,31 @@
-// using if else statement
+// // using if else statement
 
-function oppositeFaceOfDice(){
-    let ans;
-    if(n===1){
-        ans=6;
-    }else if(n===2){
-        ans=5;
-    }else if(n===3){
-        ans = 4;
-    }else if(n===4){
-        ans=3;
-    }else if(n===5){
-        ans=2;
-    }else{
-        ans=1
-    }
+// function oppositeFaceOfDice(n){
+//     let ans;
+//     if(n===1){
+//         ans=6;
+//     }else if(n===2){
+//         ans=5;
+//     }else if(n===3){
+//         ans = 4;
+//     }else if(n===4){
+//         ans=3;
+//     }else if(n===5){
+//         ans=2;
+//     }else{
+//         ans=1
+//     }
+//     return ans;
+// }
+// let n=3;
+// console.log(oppositeFaceOfDice(n))
+
+//above code is Naive Approach
+
+//[Expected Approach] Using Sum of Two Sides
+function oppositeFaceOfDice2(n1){
+    let ans = 7 - n1;
     return ans;
 }
-let n=3;
-console.log(oppositeFaceOfDice(n))
+let n1=2;
+console.log(oppositeFaceOfDice2(n1))
